2877c080748bf3375b0b19141004999ea76b0fa9
wenjuno <4373187+wenjuno@users.noreply.github.com>
2020-09-24 10:47:25 -0400

    printenv to see what environment variables are available

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+echo "========================================== PRINTENV =========================================="
+printenv
+echo "========================================== PRINTENV =========================================="
 mkdir cpp_build
 cd cpp_build
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
